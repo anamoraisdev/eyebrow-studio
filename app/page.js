55b735dd1ef41980d0f3dbf57cac56f5cfb992de
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-05-02 17:03:39 -0300

    creating section theplace

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar";
 import Services from "./components/services";
+import ThePlace from "./components/theplace";
 
 export default function Home() {
   return (
     <main className="">
       <Navbar/>
+      <ThePlace/>
       <Services/>
     </main>
   );
