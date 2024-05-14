6d11b22dfa6efb5aabf3c026963f75fa681fe54b
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-05-14 15:00:55 -0300

    add logo in navbar and footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -4,13 +4,9 @@ const Footer = () => {
   return (
     <footer className="bg-primary text-white py-8">
       <div className="flex flex-col items-center gap-20">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          {/* Logo e Direitos Autorais */}
-          <div className="flex items-center">
-            <img src="/logo.png" alt="Logo" className="h-8 mr-2" />
-          </div>
+        <div className="flex justify-center w-full gap-20 items-center">
+          <img src="/logo.png" alt="Logo" className="w-60"/>
 
-          {/* Menu de Navegação */}
           <div className="text-sm">
             <h4 className="font-bold mb-4">Menu</h4>
             <ul>
@@ -21,15 +17,15 @@ const Footer = () => {
             </ul>
           </div>
 
-          {/* Informações de Contato */}
           <div className="text-sm">
             <h4 className="font-bold mb-4">Contato</h4>
             <p>Endereço: Rua XYZ, 123</p>
             <p>Telefone: (00) 1234-5678</p>
             <p>Email: info@seusite.com</p>
+            <p>instagram: info@seusite.com</p>
           </div>
 
-          
+
         </div>
 
         <span className="text-sm border-t w-full text-center border-complement-100 border-opacity-20 ">&copy; 2024 Seu Site</span>
--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,14 +1,12 @@
 const Navbar = () => {
-  
     return (
-    
-            <main className="bg-primary w-full h-32 flex items-center justify-between text-complement-100 sm" >
-            <h1 className="w-32 mx-20 ">LOGO</h1>
+        <main className="bg-primary w-full h-36 flex items-center justify-between text-complement-100 sm" >
+            <img src="/logo.png" className="w-60 mx-20 " />
             <section className="gap-4 px-10 hidden lg:flex">
                 <a href='#services' className=" hover:text-secondary-200">
                     Serviços
                 </a>
-                <a href='#times'className=" hover:text-secondary-200">
+                <a href='#times' className=" hover:text-secondary-200">
                     Horários
                 </a>
                 <a href='#place' className=" hover:text-secondary-200">
@@ -18,10 +16,10 @@ const Navbar = () => {
                     Galeria
                 </a>
                 <a href='#about' className=" hover:text-secondary-200">
-                   Sobre
+                    Sobre
                 </a>
 
-                <a href='#contact'className=" hover:text-secondary-200">
+                <a href='#contact' className=" hover:text-secondary-200">
                     Fale conosco
                 </a>
             </section>
